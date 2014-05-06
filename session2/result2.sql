481455b27abfd2a32546c3e7d267ed90e7eb297c
Remcconen <remco-brunsveld@home.nl>
2014-05-06 15:32:43 +0200

    exercise 2.2 taak b

--- session2/result2.sql
+++ session2/result2.sql
@@ -1,3 +1,10 @@
 INSERT INTO F(x, y, violation, motivation)
 VALUES ("E", "D", 1, "E geeft ED geeft EDA. BCF zijn niet te krijgen"),
 ("D", "A", 1, "D geeft DA. BCEF zijn niet te krijgen");
+
+INSERT INTO F1(x, y, violation, motivation)
+VALUES ("E", "D", 0, "E geeft ED(gehele tabel)");
+
+INSERT INTO F2(x, y, violation, motivation)
+VALUES ("ABC", "EF", 0, "ABD geeft ABCEF(gehele tabel)"),
+("E", "A", 1, "E geeft EA. BCF niet te krijgen");
