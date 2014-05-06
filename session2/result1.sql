fe6a9bda00c187aa76937acb81a0c61d57f318d0
Remcconen <remco-brunsveld@home.nl>
2014-05-06 15:02:53 +0200

    exercise 2.1

---
+++ session2/result1.sql
@@ -0,0 +1,11 @@
+INSERT INTO Problem1(task, result, motivation)
+VALUES ('a', 0, "Er kunnen meerdere voertuigen met dezelfde capaciteit zijn"),
+('b', 1, "3"),
+('c', 0, "Bij een adres en een factuur kunnen meerdere pakketten horen"),
+('d', 1, "c, a, 7"),
+('e', 1, "4, 5"),
+('f', 0, "Een bezorger kan voor meerdere voetuigen gerechtigd zijn"),
+('g', 0, "Een voertuig kan meerdere gerechtigde bezorgers hebben, C is onbelangrijk"),
+('h', 0, "Voertuig is geen key voor bestuurder"),
+('i', 1, "Voertuig is wel een key voor capaciteit"),
+('j', 0, "Voertuig is geen key voor bestuurder");
